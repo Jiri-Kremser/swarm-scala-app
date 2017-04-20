7821674678441b1e4d06c6e9cddf760219c85cd5
Jirka Kremser <jkremser@redhat.com>
2017-04-20 15:51:00 +0200

    adding jax-rs and static content wars

--- src/main/scala/org/wildfly/swarm/examples/scala/Main.scala
+++ src/main/scala/org/wildfly/swarm/examples/scala/Main.scala
@@ -3,14 +3,25 @@ package org.wildfly.swarm.examples.scala
 import org.jboss.shrinkwrap.api.ShrinkWrap
 import org.wildfly.swarm.Swarm
 import org.wildfly.swarm.jaxrs.JAXRSArchive
+import org.wildfly.swarm.undertow.WARArchive
+
 
 object Main extends App {
+  val api: Boolean = "true" == System.getProperty("api")
+
+  println("\n\nStarting Scala App " + (if (api) "(API)" else "(static content)") + "\n\n")
 
   val container = new Swarm()
-
-  val deployment = ShrinkWrap.create(classOf[JAXRSArchive], "scala-wildfly-swarm.war")
-  deployment.addClass(classOf[ScalaResource])
-  deployment.addAllDependencies()
-  container.start().deploy(deployment)
-
+  if (api) {
+    // jax rs
+    val deployment = ShrinkWrap.create(classOf[JAXRSArchive], "scala-wildfly-swarm.war")
+    deployment.addClass(classOf[ScalaResource])
+    deployment.addAllDependencies()
+    container.start().deploy(deployment)
+  } else {
+    // static content
+    val deployment = ShrinkWrap.create(classOf[WARArchive], "scala-wildfly-swarm.war")
+    deployment.staticContent()
+    container.start.deploy(deployment)
+  }
 }
--- src/main/scala/org/wildfly/swarm/examples/scala/ScalaResource.scala
+++ src/main/scala/org/wildfly/swarm/examples/scala/ScalaResource.scala
@@ -3,7 +3,7 @@ package org.wildfly.swarm.examples.scala
 import java.util.Date
 import javax.ws.rs.{GET, Path}
 
-@Path("/")
+@Path("/api")
 class ScalaResource {
 
   @GET
